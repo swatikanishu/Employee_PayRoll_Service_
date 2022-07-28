882c5469a59c582d1f060911d440061e1abcd60e
swatikanishu <nishuswatika@gmail.com>
2022-07-29 01:24:18 +0530

    Added:welcome message added

---
+++ src/main/java/com/bridgelabz/EmployeePayRollServices.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class EmployeePayRollServices {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee service program");
+    }
+}
